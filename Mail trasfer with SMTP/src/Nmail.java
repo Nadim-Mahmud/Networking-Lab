4b55db3d2fa21a8cef025c4c44b3c9eb78da78c1
Nadim Mahmud <mahmudnadim16@gmail.com>
2019-11-28 15:35:04 +0600

    Update Nmail.java

--- Mail trasfer with SMTP/src/Nmail.java
+++ Mail trasfer with SMTP/src/Nmail.java
@@ -1,38 +1,39 @@
-////////////////// Not completed yet
+import java.io.*;
+import java.net.*;
+class Client {
+
+  public static void main(String argv[]) throws Exception
+    {
+
+         Socket s=new Socket("localhost",25);
+         		PrintWriter   os = new PrintWriter(s.getOutputStream(),true);
+         		BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+
+                os.println("HELO " + "nadim");
+                
+                os.println("MAIL FROM:"+ " bcd@nadim");
+                System.out.println( "Response2: " + br.readLine());
+                
+                os.println("RCPT TO:"+ " abc@nadim");
+                System.out.println( "Response3: " + br.readLine());
+
+                os.println("DATA");
+                System.out.println( "Response4: " + br.readLine());
+                
+                os.println("Subject: My Message");
+                os.println();
+                os.println("Hiiii");
+                os.println();
+                os.println(".");
+                
+                System.out.println( "Response5: " + br.readLine());
+                         
+                os.println("QUIT");
+                System.out.println( "Response6: " + br.readLine());
+                System.out.println( "Response7: " + br.readLine());
 
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
-public class Nmail {
-
-  public static void main(String args[]) throws Exception{
-	  
-	  Socket socket = new Socket("localhost",25);
-	  PrintWriter pr = new PrintWriter(socket.getOutputStream(),true);
-	  BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	  
-	  pr.println("HELO " + "localhost");
-	  System.out.println("Response1: " + br.readLine());
-	  
-	  pr.println("MAIL FROM:"+" abc@nadim.com");
-	  System.out.println("Response2: " + br.readLine());
-	  
-	  pr.println("RCPT TO:"+" bcd@nadim.com");
-	  System.out.println("Response3: " + br.readLine());
-
-	  pr.println("DATA");
-	  System.out.println("Response4: " + br.readLine());
-	  
-	  pr.println("Subject: MyMe");
-	  pr.println();
-	  pr.println("Hi!!!!");
-	  pr.println("");
-	  pr.println(".");
-	  System.out.println("Response4: " + br.readLine());
-
-  }
+    }
 }
